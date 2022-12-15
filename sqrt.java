8caaa4d4c99dfbe6b4fd56ae6176b2e626a50745
Grey Hat <mohammedalthaf1111@gmail.com>
2022-12-16 02:28:13 +0530

    first commit

---
+++ sqrt.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+public class sqrt
+{
+	public static void main(String args[])
+	{
+		double a,b;
+		a=10;
+		b=Math.sqrt(a);
+		System.out.println("square of"+a+"is :"+b);
+	}
+}
